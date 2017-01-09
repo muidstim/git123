57fe11eab22a0d379ee0995bb5794b04a97d1d3e
Tim Girard <tim.gir@mahidol.edu>
2017-01-09 09:33:24 +0700

    adding project

--- src/com/muids/Main.java
+++ src/com/muids/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello Git");
+        System.out.print("Hello Git - mybranch Tim");
     }
 }
